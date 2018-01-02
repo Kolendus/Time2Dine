5397813c32beed32da5d346dd28d7ad8cc7d76f4
kolendom <kolendom@ee.pw.edu.pl>
2018-01-02 21:30:54 +0100

    Spock Test Structure

---
+++ Time2Dine/test/AlgorithmTest.groovy
@@ -0,0 +1,12 @@
+import spock.lang.Specification
+
+class AlgorithmTest extends Specification {
+    def "Mutate"() {
+    }
+
+    def "CrossBreed"() {
+    }
+
+    def "GenerateChromosomes"() {
+    }
+}
