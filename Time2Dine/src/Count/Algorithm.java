003728c1eee4783476c76069899b0cee6dd5f545
kolendom <kolendom@ee.pw.edu.pl>
2018-01-06 23:17:08 +0100

    ArrayList Chromosomes

--- Time2Dine/src/Count/Algorithm.java
+++ Time2Dine/src/Count/Algorithm.java
@@ -1,15 +1,28 @@
+import java.util.ArrayList;
 
 public class Algorithm implements INAlgorithm {
 
-    public void mutate(Chromosome[] chromosomes) {
+    private ArrayList<Furniture> furList;
+    public void mutate(ArrayList<Chromosome> chromosomes) {
 
     }
 
-    public void crossBreed(Chromosome[] chromosomes) {
+    public void crossBreed(ArrayList<Chromosome> chromosomes) {
 
     }
 
     public Chromosome generateChromosomes(Canteen canteen) {
+        Chromosome chromosome = new Chromosome();
+        this.furList = new ArrayList<>();
+
+        addFurnitureToChromosome(chromosome,furList);
         return null;
+    }
+
+
+    private void addFurnitureToChromosome(Chromosome chromosome, ArrayList<Furniture> fur){
+        for(int i = 0; i < fur.size(); i++){
+            chromosome.addFurniture(fur.get(i));
+        }
     }
 }
--- Time2Dine/src/Count/INAlgorithm.java
+++ Time2Dine/src/Count/INAlgorithm.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
+
 public interface INAlgorithm {
-    void mutate(Chromosome [] chromosomes);
-    void crossBreed(Chromosome [] chromosomes);
+    void mutate(ArrayList<Chromosome> chromosomes);
+    void crossBreed(ArrayList<Chromosome> chromosomes);
     Chromosome generateChromosomes(Canteen canteen);
 }
