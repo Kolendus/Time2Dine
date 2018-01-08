87cba32c4a98236b18893f0a6019401f1d6031f0
krolakb <krolakbartlomiej@gmail.com>
2018-01-08 14:44:11 +0100

    Changed types of fields/added getters and setters.

--- Time2Dine/src/Model/Furniture.java
+++ Time2Dine/src/Model/Furniture.java
@@ -1,10 +1,10 @@
 public class Furniture {
 
     private FurnitureEnum key;
-    private double aPosition;
-    private double bPosition;
-    private double cPosition;
-    private double dPosition;
+    private int x1Position;
+    private int y1Position;
+    private int x2Position;
+    private int y2Position;
     private double furCost;
     /*Here i have not really thought about "size" of the furniture. I mean
     if we are generating  chromosome, positions generated through that method
@@ -12,22 +12,30 @@ public class Furniture {
     I think we can skip creating variable concerning the size problem.
      */
 
-    public Furniture(FurnitureEnum key, double aPosition, double bPosition, double cPosition, double dPosition, double furCost) {
+    public Furniture(FurnitureEnum key, int x1Position, int y1Position, int x2Position, int y2Position, double furCost) {
         this.key = key;
-        this.aPosition = aPosition;
-        this.bPosition = bPosition;
-        this.cPosition = cPosition;
-        this.dPosition = dPosition;
+        this.x1Position = x1Position;
+        this.y1Position = y1Position;
+        this.x2Position = x2Position;
+        this.y2Position = y2Position;
         this.furCost = furCost;
     }
 
-    public Furniture(FurnitureEnum key, double aPosition, double bPosition) {
+    public Furniture(FurnitureEnum key, int x1Position, int y1Position) {
         this.key = key;
-        this.aPosition = aPosition;
-        this.bPosition = bPosition;
+        this.x1Position = x1Position;
+        this.y1Position = y1Position;
     }
 
     public double getFurCost() {
         return furCost;
+    }
+
+    public int getX1Position() {
+        return x1Position;
+    }
+
+    public int getY1Position() {
+        return y1Position;
     }
 }
