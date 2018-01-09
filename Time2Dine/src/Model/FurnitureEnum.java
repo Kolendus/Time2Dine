abbcedabee957f6f74cf41a9160c211a069a45bc
krolakb <krolakbartlomiej@gmail.com>
2018-01-09 14:28:58 +0100

    Added some cases, added numberOfFurniture method.

--- Time2Dine/src/Model/FurnitureEnum.java
+++ Time2Dine/src/Model/FurnitureEnum.java
@@ -13,6 +13,8 @@ public enum FurnitureEnum {
                 case SMALLBENCH: return 99;
                 case BIGBENCH: return 198;
                 case LOWILUM: return 18;
+                case MEDILUM: return 18;
+                case STRONGILUM: return 18;
             }
             return -1;
         }
@@ -26,6 +28,18 @@ public enum FurnitureEnum {
             case SMALLBENCH: return 32;
             case BIGBENCH: return 32;
             case LOWILUM: return 18;
+            case MEDILUM: return 18;
+            case STRONGILUM: return 18;
+        }
+        return -1;
+    }
+
+    public int getNumberOfFurniture(FurnitureEnum furEnum){
+        int counter = 0;
+        for(FurnitureEnum f : FurnitureEnum.values()) {
+            if ( f.name().equals(furEnum.name()) )
+                return counter;
+            counter++;
         }
         return -1;
     }
