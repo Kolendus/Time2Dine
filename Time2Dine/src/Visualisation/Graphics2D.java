4649e298e9a5a05e7da1a0f316bcf3ae6c5e80b8
krolakb <krolakbartlomiej@gmail.com>
2018-01-16 20:54:49 +0100

    Added one constructor and methods, which help drawing.

--- Time2Dine/src/Visualisation/Graphics2D.java
+++ Time2Dine/src/Visualisation/Graphics2D.java
@@ -15,8 +15,23 @@ public class Graphics2D extends JPanel {
     private BufferedImage table8 = null;
     private BufferedImage lamp = null;
     private BufferedImage bench = null;
+    private Canteen canteen;
+    private Chromosome chromosome;
 
     private BufferedImage savePicture;
+
+    public Graphics2D(Canteen canteen ,Chromosome chromosome) {
+        this();
+        this.canteen = canteen;
+        this.chromosome = chromosome;
+        this.setBounds(6, 6, 586, 506);
+
+        savePicture = new BufferedImage(this.getSize().width, this.getSize().height, BufferedImage.TYPE_INT_ARGB);
+        Graphics g = savePicture.createGraphics();
+        this.paint(g);
+        g.dispose();
+        repaint();
+    }
 
     public Graphics2D() {
         this.setVisible(true);
@@ -31,13 +46,6 @@ public class Graphics2D extends JPanel {
         } catch (IOException e) {
             System.out.println("Nie znaleziono obrazka!");
         }
-        this.setBounds(6, 6, 586, 506);
-
-        savePicture = new BufferedImage(this.getSize().width, this.getSize().height, BufferedImage.TYPE_INT_ARGB);
-        Graphics g = savePicture.createGraphics();
-        this.paint(g);
-        g.dispose();
-        repaint();
     }
 
     public void visualize(Chromosome chromosome) {
@@ -50,7 +58,7 @@ public class Graphics2D extends JPanel {
         this.setBounds(6, 6, 586, 506);
         super.paintComponent(g);
 
-        int point1 = 0;
+        /*int point1 = 0;
         int point2 = 250;
         int point3 = 400;
         int dt = 1; //cause of line thickness, needed to fill polygon properly
@@ -64,9 +72,12 @@ public class Graphics2D extends JPanel {
         Polygon filledPoly = new Polygon(xFilledPoly, yFilledPoly, xFilledPoly.length);
 
         g.setColor(Color.BLACK);
-        g.drawPolygon(borderPoly);
+        g.drawPolygon(borderPoly);*/
 
-        drawRotate(chair, 0, 27, 30, g, 1, 1);
+        drawCanteen(g);
+        drawFurnitures(g);
+
+        /*drawRotate(chair, 0, 27, 30, g, 1, 1);
         drawRotate(chair, 0, 52, 30, g, 1, 1);
         drawRotate(chair, 0, 77, 30, g, 1, 1);
         drawRotate(chair, 0, 102, 30, g, 1, 1);
@@ -76,11 +87,29 @@ public class Graphics2D extends JPanel {
         drawRotate(chair, 0, 202, 30, g, 1, 1);
         drawRotate(bench, 0, 23, 55, g, 1, 1);
         drawRotate(bench, 0, 121, 55, g, 1, 1);
-        drawRotate(lamp, 0, 200, 200, g, 1, 1);
+        drawRotate(lamp, 0, 200, 200, g, 1, 1);*/
 
     }
 
-    private static void drawRotate(BufferedImage img, double angle, int locationX, int locationY, Graphics g, double xScale, double yScale) {
+    private void drawFurnitures(Graphics g) {
+        for(Furniture f : chromosome.getFurList()) {
+            BufferedImage img = null;
+            switch(f.getKey().name()){
+                case "FOURPTABLE": img = table4; break;
+                case "SIXPTABLE": img = table6; break;
+                case "EIGHTPTABLE": img = table8; break;
+                case "SMALLCHAIR": img = chair; break;
+                case "SMALLBENCH": img = bench; break;
+                case "BIGBENCH": img = bench; break;
+                case "LOWILUM": img = lamp; break;
+                case "MEDILUM": img = lamp; break;
+                case "STRONGILUM": img = lamp; break;
+            }
+            drawRotate(img,0, f.getX1Position(), f.getY1Position(), g, 1, 1); // TODO: change furniture;
+        }
+    }
+
+    private void drawRotate(BufferedImage img, double angle, int locationX, int locationY, Graphics g, double xScale, double yScale) {
         AffineTransform tx = AffineTransform.getRotateInstance(Math.toRadians(angle), img.getWidth() / 2, img.getHeight() / 2);
         tx.scale(xScale, yScale);
         AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
@@ -88,7 +117,7 @@ public class Graphics2D extends JPanel {
     }
 
     /*Błąd w rzędu  <= 0,5%Punkt startu (0,0)*/
-    private static void drawCanteen(Canteen canteen, Graphics g) {
+    private void drawCanteen(Graphics g) {
         double meterToPixel = 37.74;
         int point2 = (int) (meterToPixel * canteen.gettWall());
         int point3 = (int) ((meterToPixel * (canteen.getlWall() - canteen.getrWall())));
